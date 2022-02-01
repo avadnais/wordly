2af6f28e5c6abac1d5166401d778d8be3f5a5125
Andy <avadnais@oswego.edu>
2022-01-31 23:04:21 -0500

    ga

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Game from "./components/Game";
 import { StyledApp } from "./components/styles/App.styled";
-
+import ReactGA from 'react-ga';
 import GlobalStyles from "./components/styles/Global";
 
 function App(props) {
+
+  const TRACKING_ID = "UA-86816534-2";
+  ReactGA.initialize(TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
 
   const {state, dispatch} = props;
 
